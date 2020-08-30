cd89b1cf6e2acbff563f71694771288b66fbb977
kennetnnah <44839686+techagentng@users.noreply.github.com>
2020-08-30 20:03:01 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -21,15 +21,26 @@
 // }
 
 
-let change = document.querySelector('.card')
-let black = document.querySelector('.black')
-let tag = document.querySelector('.tagline')
-let tag2 = document.querySelector('.tag')
-let follow = document.getElementsById('test')
+// let change = document.querySelector('.card')
+// let black = document.querySelector('.black')
+let gtest = document.querySelectorAll('.gtest')
+let card = document.querySelector('.card')
+// let tag = document.querySelector('.tagline')
+// let tag2 = document.querySelector('.tag')
+// let follow = document.querySelector('.gtest')
+// let follow1 = document.querySelector('.gtest1')
+// let follow2 = document.querySelector('.gtest2')
 function clickEvent(){
-    change.classList.toggle('addBg');
-    black.classList.toggle('white');
-    tag.classList.toggle('white');
-    tag2.classList.toggle('white');
-    follow.classList.add('white');
+
+    for(let i=0; i<gtest.length; i++){
+        gtest[i].classList.toggle('white')
+    }
+    
+    card.classList.toggle('wc');
+    // black.classList.toggle('white');
+    // tag.classList.toggle('white');
+    // tag2.classList.toggle('white');
+    // follow.classList.toggle('white');
+    // follow1.classList.toggle('white');
+    // follow2.classList.toggle('white');
 }
